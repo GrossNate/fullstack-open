dba244e6c2189831e939f0e9f1ebefb77a4eb7e0
Nathan Gross <nathan.gross@gmail.com>
2024-11-15 09:23:15 -0600

    unicafe step 2

--- part1/unicafe/src/App.tsx
+++ part1/unicafe/src/App.tsx
@@ -6,12 +6,30 @@ const Button = (
     <button onClick={handler}>{buttonText}</button>
   );
 }
+
+const Stats = ({good, neutral, bad} : {good:number, neutral:number, bad:number}) => {
+  const all = good + neutral + bad;
+  const avg = (good - bad) / all;
+  const positive = good / all;
+
+  return (
+    <>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average: {Number.isNaN(avg)? "n.a." : avg}</p>
+      <p>positive: {Number.isNaN(positive) ? "n.a." : positive.toString() + " %"}</p>
+    </>
+  );
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  
   return (
     <>
       <h2>give feedback</h2>
@@ -19,9 +37,7 @@ const App = () => {
       <Button handler={() => { setNeutral(neutral + 1) }} buttonText="neutral" />
       <Button handler={() => { setBad(bad + 1) }} buttonText="bad" />
       <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Stats good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
