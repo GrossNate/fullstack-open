94cc5ba67a41fdbb158121ce79829365644007ec
Nathan Gross <nathan.gross@gmail.com>
2024-11-16 14:06:40 -0600

    phonebook step 3

--- part2/phonebook/src/App.tsx
+++ part2/phonebook/src/App.tsx
@@ -1,46 +1,58 @@
 import { useState } from 'react'
 
 interface Person {
-  name: string
+  name: string,
+  phoneNumber: string
 }
 
 const DisplayNumbers = ({ persons }: { persons: Person[] }) => {
   return (
-    <ul>
-      {persons.map(({ name }) => {
+    <table>
+    <tbody>
+      {persons.map(({ name, phoneNumber }) => {
         return (
-          <li key={name}>
-            {name} <br />
-          </li>
+          <tr key={name}>
+          <td>
+            {name}
+          </td>
+          <td>
+            {phoneNumber}
+          </td>
+          </tr>
         )
       })
       }
-    </ul>
+      </tbody>
+    </table>
   );
 };
 
 const AddNumberForm = ({ persons, setPersons }: { persons: Person[], setPersons: (persons: Person[]) => void }) => {
   const [newName, setNewName] = useState('');
+  const [newPhone, setNewPhone] = useState('');
 
   const handleSubmitForm = (event: React.ChangeEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (persons.map(({ name }) => name.toLowerCase()).includes(newName.toLowerCase())) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons(persons.concat({ name: newName }));
+      setPersons(persons.concat({ name: newName, phoneNumber: newPhone }));
       event.target.reset();
       setNewName("");
     }
   };
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNewName(event.target.value);
+  const handleInputChange = (controller: React.Dispatch<React.SetStateAction<string>>) => {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {controller(event.target.value)};
   }
 
   return (
     <form onSubmit={handleSubmitForm}>
       <div>
-        name: <input onChange={handleInputChange} />
+        name: <input onChange={handleInputChange(setNewName)} />
+      </div>
+      <div>
+        phone: <input onChange={handleInputChange(setNewPhone)} />
       </div>
       <div>
         <button type="submit">add</button>
@@ -52,7 +64,7 @@ const AddNumberForm = ({ persons, setPersons }: { persons: Person[], setPersons:
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phoneNumber: '555-1212' }
   ]);
 
   return (
