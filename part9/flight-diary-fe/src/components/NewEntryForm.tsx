00fe57eee61d742e48e7f1e9ee845389ebe4ff6d
Nathan Gross <nathan.gross@gmail.com>
2024-12-11 08:23:10 -0600

    flight diaries setup

---
+++ part9/flight-diary-fe/src/components/NewEntryForm.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+const NewEntryForm = () => {
+  const [dateValue, setDateValue] = useState('')
+  const [ visibilityValue, setVisibilityValue ] = useState('')
+  const [ weatherValue, setWeatherValue ] = useState('')
+  
+  return (
+    <form>
+
+    </form>
+  )
+}
+
+export default NewEntryForm
